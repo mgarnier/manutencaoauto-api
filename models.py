8ef82121dc0efb4a460cdac9f98d3c044c85e88e
Marcelo Garnier <mgarnier@gmail.com>
2026-04-11 12:39:28 -0300

    Mapeamento da tabela Servico.

--- models.py
+++ models.py
@@ -7,3 +7,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Servico(db.Model):
+    __tablename__ = 'servico'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), unique=True, nullable=False)
+    frequencia = db.Column(db.Integer, nullable=False)
+    preco = db.Column(db.Numeric(10, 2), nullable=False)
+
+    def __repr__(self):
+        return f'<Servico {self.nome}>'
