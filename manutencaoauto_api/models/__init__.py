17b4fb1e4415a8b64d385b587000693bb2754c53
Marcelo Garnier <mgarnier@gmail.com>
2026-04-12 11:57:51 -0300

    [Refatoração] Alteração de nome do arquivo com mapeamento da entidade ManutencaoServico.

--- manutencaoauto_api/models/__init__.py
+++ manutencaoauto_api/models/__init__.py
@@ -1,4 +1,4 @@
-from manutencaoauto_api.models.associacao import ManutencaoServico
+from manutencaoauto_api.models.manutencao_servico import ManutencaoServico
 from manutencaoauto_api.models.manutencao import Manutencao
 from manutencaoauto_api.models.servico import Servico
 
